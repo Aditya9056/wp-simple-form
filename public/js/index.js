1fef337ee513ca1bb15874f261bf69b12020f16e
Aditya Jain <vvasujain@gmail.com>
2021-06-18 14:01:35 +0530

    removed commented code

--- public/js/index.js
+++ public/js/index.js
@@ -43,25 +43,3 @@ jQuery( document ).ready(
 		}
 	),
 );
-
-// function($) {
-// $( '.simple-form' ).on(
-// 'submit',
-// function(e) {
-// e.preventDefault();
-//
-// let form = $( this );
-//
-// $.post(
-// form.attr( 'action' ),
-// form.serialize(),
-// function(data) {
-// alert( 'This is data returned from the server ' + data );
-// },
-// 'json'
-// );
-// }
-// );
-//
-// }
-
